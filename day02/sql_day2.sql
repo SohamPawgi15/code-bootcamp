7a047ecfb38e8a7e05b19e18c4134019377311bd
SohamPawgi15 <spawgi@asu.edu>
2025-05-17 17:56:08 -0700

    Day 2: SQL practice

---
+++ day02/sql_day2.sql
@@ -0,0 +1,10 @@
+-- Problem name: SELECT ALL
+SELECT * from CITY
+
+-- Problem name: SELECT BY ID
+SELECT * FROM CITY
+WHERE ID = 1661; 
+
+-- Problem name: Weather Observation Station 1
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN'; 
