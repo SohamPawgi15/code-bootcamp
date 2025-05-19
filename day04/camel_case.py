e3361cdefd6c4c051e409526361462a8433d7f35
SohamPawgi15 <spawgi@asu.edu>
2025-05-18 21:09:42 -0700

    Added camelCase to snake_case converter code

---
+++ day04/camel_case.py
@@ -0,0 +1,9 @@
+camel = input("camelCase: ")
+snake = ""
+for c in camel:
+    if c.isupper():
+        snake += "_" + c.lower()
+    else:
+        snake += c 
+        
+print("snake_case:", snake)
